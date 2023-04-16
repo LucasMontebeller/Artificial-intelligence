85dde5ed90c35253129d15d08dbeb7a42a3dee28
Lucas Montebeller <montebellerlucas@gmail.com>
2023-04-16 12:33:26 -0300

    Adicionando POC para testar algumas libs de imagem

---
+++ Pocs/Image-Processing/src/opencv_pil_tests.py
@@ -0,0 +1,71 @@
+import os
+from PIL import Image as PILImage
+from PIL import ImageOps as ops
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from enum import Enum, auto
+
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+IMAGE_DIR = os.path.join(SCRIPT_DIR, '..', 'Images')
+os.chdir(SCRIPT_DIR)
+
+class Lib(Enum):
+    PIL = auto()
+    OPENCV = auto()
+
+class Image:
+
+    def __init__(self, name):
+        self.path = os.path.join(IMAGE_DIR, f'{name}.png')
+
+    def download(self, link, name):
+        if not os.path.exists(self.path):
+            os.system(f'wget {link} -O {name}.png -P {IMAGE_DIR}')
+        else:
+            print("Já existe uma imagem com este nome !")
+
+    def prepare(self, lib):
+        if os.path.exists(self.path):
+            match lib:
+                case Lib.PIL:
+                    image = PILImage.open(self.path)
+                    image_array = np.asarray(image)
+                case Lib.OPENCV:
+                    image_array = cv2.imread(self.path)
+
+        return image_array
+    
+    @staticmethod
+    def cut(array, height, width, channel=None):
+        new_image = array.copy()
+        new_image = new_image[height[0] : height[1], width[0] : width[1]]
+        if channel is None:
+            return new_image
+        
+        return new_image[:,:, channel]
+    
+    @staticmethod
+    def super_position(array, height, width, channel):
+        new_image = array.copy()
+        new_image[height[0] : height[1], width[0] : width[1]] = channel
+        return new_image
+    
+horse = Image('horse')#.prepare(Lib.PIL)
+# plt.imshow(horse)
+# plt.show()
+
+# new_horse = Image.cut(horse,[17, 158], [196, 297], 2)
+# plt.imshow(new_horse)
+# plt.show()
+
+# new_horse = Image.super_position(horse,[100, 200], [50, 150], 170)
+# plt.imshow(new_horse)
+# plt.show()
+
+# flip
+new_horse = PILImage.open(horse.path) 
+# new_horse = ops.flip(new_horse)
+plt.imshow(new_horse)
+plt.show()
+
