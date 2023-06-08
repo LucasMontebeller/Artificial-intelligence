a4d61129e65d5904dd19e269f7f2c2594b513852
Lucas Montebeller <montebellerlucas@gmail.com>
2023-06-08 14:19:45 -0300

    Incluindo comparativo do numpy

--- yield.py
+++ yield.py
@@ -1,17 +1,24 @@
-import time 
+import time
+import numpy as np
 SIZE_LIST = int(1e8)
 
 def list_generator():
-    lista = []
-    for i in range(SIZE_LIST):
-        lista.append(i)
+    # lista = []
+    # for i in range(SIZE_LIST):
+    #     lista.append(i)
 
-    return lista
+    # return lista
+    return list(range(SIZE_LIST))
 
 def list_generator_w_yield():
-    for i in range(SIZE_LIST):
-        yield i
+    # for i in range(SIZE_LIST):
+    #     yield i
+    return (x for x in range(SIZE_LIST))
 
+def list_generator_w_numpy():
+    return np.arange(SIZE_LIST)
+
+# -------------------------------------------------
 tempo_inicial = time.time()
 for i in list_generator():
     if i == 1000:
@@ -27,3 +34,12 @@ for i in list_generator_w_yield():
         break
 tempo_final = time.time()
 print(f"Tempo gasto com yield: {tempo_final - tempo_inicial}")
+
+print('-' * 100)
+
+tempo_inicial = time.time()
+for i in list_generator_w_numpy():
+    if i == 1000:
+        break
+tempo_final = time.time()
+print(f"Tempo gasto com numpy: {tempo_final - tempo_inicial}")
