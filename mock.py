7cedad27823a95777f94cc039cd38fb7233666b6
Lucas Montebeller <montebellerlucas@gmail.com>
2023-06-17 18:23:36 -0300

    Alterando mock

--- mock.py
+++ mock.py
@@ -1,5 +1,16 @@
-def mock_func():
-    lista = [1,2,3]
-    return lista
+import random as rd
+import sys
 
-print(mock_func())
+def mock_func(size):
+    return [rd.randint(0, 10) for _ in range(size)]
+
+def main(size):
+    print(mock_func(size))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        size = int(sys.argv[1])
+    else: 
+        size = 10
+    
+    main(size)
